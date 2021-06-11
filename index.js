060356c2c38b90d8490e281ed50118f9a5a932a7
Caitlyn <meszaros.caitlyn@gmail.com>
2021-06-11 08:26:49 -0400

    commit tree 53ff59eb0608932638f71465a0d3641e605f9ce0

---
+++ index.js
@@ -0,0 +1,15 @@
+//function expression called divide
+//divide dives 200 by 100 = 20
+const divide = function() {
+    return (200/10);
+  };
+
+//has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+const square = (num1) => num1 * num1;
+
+
+//has an arrow function called add
+//add arrow function takes two placeholders and adds them together
+
+const add = (num3, num4) => num3 + num4;
